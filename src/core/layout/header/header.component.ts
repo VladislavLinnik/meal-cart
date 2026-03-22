1ba5a8411d10e1835ff386df6a8615cb1c779526
vladislav <vladislav.linnik.1999@gmail.com>
2026-03-23 01:00:30 +0200

    Optimized for mobile

--- src/core/layout/header/header.component.ts
+++ src/core/layout/header/header.component.ts
@@ -1,10 +1,13 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CartService } from '../../services/cart.service';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { heroShoppingCart } from '@ng-icons/heroicons/outline';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule],
+  imports: [RouterModule, NgIcon],
+  viewProviders: [provideIcons({ heroShoppingCart })],
   templateUrl: './header.component.html',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/features/dish/components/dish-card/dish-card.component.ts
+++ src/features/dish/components/dish-card/dish-card.component.ts
@@ -1,12 +1,14 @@
 import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
 import { DishIngredientPipe } from '../../pipes/dish-ingredient.pipe';
-import { NgIcon } from '@ng-icons/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
 import { Dish } from '../../../../core/models/dish.model';
 import { RouterLink } from '@angular/router';
+import { heroShoppingCart } from '@ng-icons/heroicons/outline';
 
 @Component({
   selector: 'app-dish-card',
-  imports: [DishIngredientPipe, NgIcon, RouterLink],
+  imports: [DishIngredientPipe, NgIcon, RouterLink, NgIcon],
+  viewProviders: [provideIcons({ heroShoppingCart })],
   templateUrl: './dish-card.component.html',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
